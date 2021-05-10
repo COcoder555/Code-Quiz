bdce5ff76313f6d7c9155f9ec7f60a255f06ef18
COcoder555 <cocoder555@gmail.com>
2021-05-10 17:45:55 -0600

    Created and styled landing page

---
+++ script.js
@@ -0,0 +1,17 @@
+// The start button should bring you to the first question with multiple choice options
+// Whe the start button the timer should start. 
+// The whatever the user chooses for thier answer should be localy stored using "local starage...you will need to strigify it
+// If the user returns a correct answer  there should be a return message of CORRECT
+// If the user returns a wrong answer there should be a return of WRONG
+// before storing the value locally (  you will use the user input to get tally scores and get infor for high scores)
+var question1 = "What is a Boolean?"
+var question2 = "What syntax do we use to declare an Object"
+var question3 = "What syntax do we use to declare an Array"
+var question4 = "Sting values should be stored within____ when assigned to a variable"
+var question5 = "Only____ can be stored localy"
+
+START.addEventListener("click",fucnction(){
+
+}
+
+)
