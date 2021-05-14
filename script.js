ffcc07c1059cf4be64f303f6b3113410ef784735
COcoder555 <cocoder555@gmail.com>
2021-05-13 20:07:10 -0600

    Created End Game function and changed element in answer array

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var questions = ["What is a Boolean?", "What syntax do we use to store an Object
 "What do we use to store a String?","Only____ can be stored localy"]
 
 var answers = [["A function", "A syntax error","a binary variable, having two possible values called “true” and “false”",
-"A programming language","A measurment of processing speed"],["{", "[",";","#","$"],["$","[",";","#","}"],["Quotation Marks","<>","Parenthesis","{}","Question Marks"],
+"A programming language","A measurment of processing speed"],["{", "[",";","#","$"],["$","[",";","#","}"],["Quotation Marks","<>","Parenthesis","{}","[]"],
 ["number","Loops","functions","Strings","HTML"]]
 var $btn1 = document.createElement("button"); 
 var $btn2 = document.createElement("button");
@@ -28,9 +28,9 @@ var div = document.getElementById("Answer1");
 var $START = document.getElementById("START")
 var timeEl = document.getElementById("timer");
 var secondsLeft = 500;
-
+var timerInterval;
 function setTime() {
-    var timerInterval = setInterval( function(){
+     timerInterval = setInterval( function(){
         secondsLeft --;
         timeEl.textContent =  "Time:" + secondsLeft
         if( secondsLeft===0){
@@ -78,11 +78,11 @@ function checkAns(event) {
         // nextQuestion();
         // secondsLeft-=20; 
     } 
-    if($Question.textContent==="What do we use to store a String?" && element.textContent==="Parenthesis"){
+    if($Question.textContent==="What do we use to store a String?" && element.textContent==="Quotation Marks"){
         nextQuestion();
         
 
-    } else if($Question.textContent==="What do we use to store a String?" &&  element.textContent !=="Parenthesis"){
+    } else if($Question.textContent==="What do we use to store a String?" &&  element.textContent !=="Quotation Marks"){
         // nextQuestion();
         // secondsLeft -=20;
     }
@@ -115,18 +115,37 @@ btnM[4].addEventListener("click",checkAns)
 nextQuestion();
 var score 
 var data = localStorage.getItem("High-Score")
+var scoreList 
 function ENDGAME() {
+    clearInterval(timerInterval);
+    timeEl.textContent ="";
+    div.textContent ="";
+    $Question.textContent ="SCORES";
     score = secondsLeft;
     var initials = prompt("Enter your Initials");
     var usrScore = { score:score, initials:initials}
-    if(data){data =JSON.parse(localStorage.getItem("High-Score"))
+    if(data){data =JSON.parse(localStorage.getItem("High-Score"));
     data.push(usrScore)
     localStorage.setItem("High-Score", JSON.stringify(data))}
     else{
-        usrScore = JSON.stringify([usrScore])
-        localStorage.setItem("High-Score",usrScore)
+        usrScore = JSON.stringify([usrScore]);
+        localStorage.setItem("High-Score",usrScore);
     }
+  scoreList = JSON.parse(localStorage.getItem("High-Score"));
+  for(let i = 0; i < scoreList.length; i++){
+      var scoreLine = document.createElement("p");
+     scoreLine.textContent = scoreList[i].initials + ":" + scoreList[i].score 
+     div.append(scoreLine);
+
+  }
 }
+
+
+
+
+
+
+
 
 // $Answer[0].addEventListener("click",checkAns)
 // $Answer[1].addEventListener("click",checkAns)
